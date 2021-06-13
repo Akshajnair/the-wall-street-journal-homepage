de195f7e017807011fa9f1dbae67da661096b300
Akshaj Nair <34983749+Akshajnair@users.noreply.github.com>
2021-06-13 12:32:02 +0530

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -2,26 +2,26 @@ export const newsconfig = {
   column1: {
     lang: "en",
     country: "us",
-    token: "b8df3f585de15fcb48dc56a765172326", //token1
+    token: "89dd35e06b0a156f160fdd6df7f4103e", //token1
   },
   column2: {
     lang: "en",
     country: "us",
     topic: "business",
-    token: "b8df3f585de15fcb48dc56a765172326", //token1
+    token: "f4e2d9c83744f7d1e91881ca318d3cd8", //token2
   },
   column3: {
     lang: "en",
     country: "us",
     topic: "technology",
-    token: "79fac84a8b04b258bf0b1e89f526a26e", //token2 
+    token: "1495ad34d9e81774547be549c1ff242c", //token3 
   },
   column4: {
     lang: "en",
     country: "us",
     q: "opinion",
     sortby: "publishedAt",
-    token: "79fac84a8b04b258bf0b1e89f526a26e", //token2
+    token: "a96e0ca90ee8c28d855d619b3080063d", //token4
   },
 };
-// free account cant make more thean 3 req at a time.
+// free account cant make more thean 3 req at a time and max 100 req in a day
